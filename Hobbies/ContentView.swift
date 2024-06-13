958987a9778b68a58e6a428ccefb3a9db8dcf07a
Tim-engineer <matlakt@outlook.com>
2024-06-13 20:09:10 +0200

    Started the project from scratch and added some mockups

--- Hobbies/ContentView.swift
+++ Hobbies/ContentView.swift
@@ -7,70 +7,44 @@
 
 import SwiftUI
 
+struct HobbyItem: Identifiable {
+    let id = UUID()
+    
+    let name: String
+    let time: Double
+}
+
+@Observable
+class Hobbies {
+    var items = [HobbyItem]()
+}
+
 struct ContentView: View {
+    @State private var hobbies = Hobbies()
+    
     var body: some View {
         NavigationStack {
-            ScrollView {
-                HobbyCell(symbolSF: "paintpalette", headline: "Create", caption: "Make something", background: LinearGradient(gradient: Gradient(colors: [.orange.opacity(0.6), .orange.opacity(0.2)]), startPoint: .top, endPoint: .bottom))
-                HobbyCell(symbolSF: "figure.walk", headline: "Move", caption: "Get outside", background: LinearGradient(gradient: Gradient(colors: [.purple.opacity(0.6), .purple.opacity(0.2)]), startPoint: .top, endPoint: .bottom))
-                HobbyCell(symbolSF: "graduationcap", headline: "Practice", caption: "Imrove Skills", background: LinearGradient(gradient: Gradient(colors: [.cyan.opacity(0.6), .cyan.opacity(0.2)]), startPoint: .top, endPoint: .bottom))
-                HobbyCell(symbolSF: "tv", headline: "Relax", caption: "Zone out", background: LinearGradient(gradient: Gradient(colors: [.blue.opacity(0.6), .blue.opacity(0.2)]), startPoint: .top, endPoint: .bottom))
+            List {
+                ForEach(hobbies.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
             }
-            .navigationTitle("Today")
+            .navigationTitle("Hobbies")
+            .toolbar {
+                Button("Create Hobby", systemImage: "plus") {
+                    let hobby = HobbyItem(name: "Hobby Created...", time: 15)
+                    hobbies.items.append(hobby)
+                }
+            }
         }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        hobbies.items.remove(atOffsets: offsets)
     }
 }
 
 #Preview {
     ContentView()
-}
-
-struct HobbyCell: View {
-    let symbolSF: String
-    let headline: String
-    let caption: String
-    let background: LinearGradient
-    
-    @State private var amount = 0.0
-    
-    var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: symbolSF)
-                    .font(.system(size: 33))
-                VStack(alignment: .leading) {
-                    Text(headline)
-                        .font(.headline)
-                    Text(caption)
-                        .font(.caption)
-                }
-                Spacer()
-            }
-            Button {
-                
-            } label: {
-                HStack {
-                    Label("Bird Watch", systemImage: "binoculars")
-                    Spacer()
-                    Stepper("Amount", value: $amount)
-                        .labelsHidden()
-                        
-                }
-            }
-            .tint(.primary)
-            Button {
-                
-            } label: {
-                Label("Add", systemImage: "plus")
-                    .padding(.horizontal)
-            }
-            .buttonBorderShape(.capsule)
-            .padding(.top)
-            .tint(.secondary)
-        }
-        .padding()
-        .background(background, in: RoundedRectangle(cornerRadius: 20))
-        .padding(.horizontal)
-        .buttonStyle(.bordered)
-    }
 }
